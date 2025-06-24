288d914a46885dd626af136530cee1226b9727fd
leanderlul <lele41613@gmail.com>
2025-06-24 13:20:16 +0200

    changes

--- src/main/java/com/example/demo/serviceController.java
+++ src/main/java/com/example/demo/serviceController.java
@@ -3,16 +3,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-public class DemoApplication {
+@org.springframework.stereotype.Controller
+public class Controller {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Controller.class, args);
 	}
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
+	}
+	@GetMapping("/")
+	public String index() {
+		return "index";
 	}
 }
