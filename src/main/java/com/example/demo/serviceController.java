e656da9d0196c3fd15cf512838c41750263929ff
leanderlul <lele41613@gmail.com>
2025-06-26 14:15:09 +0200

    Fertiger Taschenrechner mit Landingpage in javascript

--- src/main/java/com/example/demo/serviceController.java
+++ src/main/java/com/example/demo/serviceController.java
@@ -22,35 +22,4 @@ public class serviceController {
 	}
 }
 
-	//@PostMapping("/hello")
-	//public String hello(Model model,
-	//					@RequestParam(value = "zahl1") int zahl1,
-	//					@RequestParam(value = "zahl2") int zahl2,
-	//					@RequestParam(value = "rechenzeichen") String rechenzeichen) {
-	//	double ergebnis;
-//
-	//	switch (rechenzeichen) {
-	//		case "+":
-	//			ergebnis = zahl1 + zahl2;
-	//			break;
-	//		case "-":
-	//			ergebnis = zahl1 - zahl2;
-	//			break;
-	//		case "*":
-	//			ergebnis = zahl1 * zahl2;
-	//			break;
-	//		case "/":
-	//			ergebnis = (zahl2 != 0) ? zahl1 / zahl2 : Double.NaN;
-	//			break;
-	//		default:
-	//			ergebnis = Double.NaN;
-	//	}
-//
-	//	if (rechenzeichen.equals("/") && zahl2 == 0) {
-	//		model.addAttribute("fehler", "Division durch 0 ist nicht erlaubt.");
-	//		return "hello";
-	//	}
-	//	model.addAttribute("ergebnis", ergebnis);
-//
-	//	return "hello";
-	//}
+
