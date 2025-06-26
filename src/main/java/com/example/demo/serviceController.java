1eb0ece24edace2e90ec65920188fd908c664b77
leanderlul <lele41613@gmail.com>
2025-06-26 14:11:53 +0200

    Fertiger Taschenrechner mit Landingpage in javascript

--- src/main/java/com/example/demo/serviceController.java
+++ src/main/java/com/example/demo/serviceController.java
@@ -1,21 +1,56 @@
 package com.example.demo;
+import org.springframework.ui.Model;
 import org.springframework.boot.SpringApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
-@org.springframework.stereotype.Controller
+@Controller
 public class serviceController {
-	public static void main(String[] args) {
-		SpringApplication.run(serviceController.class, args);
+
+	@PostMapping("/Rechner")
+	public String rechner(){
+		return "rechner";
 	}
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
+
 	@GetMapping("/")
-	public String index() {
-		return "index";
+	public String landingpage() {
+		return "landingpage";
 	}
 }
+
+	//@PostMapping("/hello")
+	//public String hello(Model model,
+	//					@RequestParam(value = "zahl1") int zahl1,
+	//					@RequestParam(value = "zahl2") int zahl2,
+	//					@RequestParam(value = "rechenzeichen") String rechenzeichen) {
+	//	double ergebnis;
+//
+	//	switch (rechenzeichen) {
+	//		case "+":
+	//			ergebnis = zahl1 + zahl2;
+	//			break;
+	//		case "-":
+	//			ergebnis = zahl1 - zahl2;
+	//			break;
+	//		case "*":
+	//			ergebnis = zahl1 * zahl2;
+	//			break;
+	//		case "/":
+	//			ergebnis = (zahl2 != 0) ? zahl1 / zahl2 : Double.NaN;
+	//			break;
+	//		default:
+	//			ergebnis = Double.NaN;
+	//	}
+//
+	//	if (rechenzeichen.equals("/") && zahl2 == 0) {
+	//		model.addAttribute("fehler", "Division durch 0 ist nicht erlaubt.");
+	//		return "hello";
+	//	}
+	//	model.addAttribute("ergebnis", ergebnis);
+//
+	//	return "hello";
+	//}
